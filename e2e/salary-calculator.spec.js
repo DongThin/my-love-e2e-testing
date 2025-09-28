5014c73d9cfa060da429c3baf5db91e4e4f9fc5c
Thin Pham <thin.pham0002@gmail.com>
2025-09-29 00:14:00 +0700

    apply data-driven approach for salary calculator cases

--- e2e/salary-calculator.spec.js
+++ e2e/salary-calculator.spec.js
@@ -10,7 +10,7 @@ let luongNet;
 let grossSangNetBtn;
 let thueTNCN;
 
-beforeEach(async ({page}) => {
+beforeEach(async ({ page }) => {
 
     await page.goto('https://thin-pham.web.app/');
     incomeInput = page.locator('#income');
@@ -22,112 +22,22 @@ beforeEach(async ({page}) => {
 });
 
 
-describe('Homepage test UI', function () {
+describe('Salary Calculator - Gross to Net', function () {
 
-    test('Homepage has title', async ({ page }) => {
+    test('Display homepage title correctly', async ({ page }) => {
         await expect(page.locator('h1')).toHaveText('Công cụ tính lương Gross sang Net và ngược lại [Chuẩn 2025]');
     });
 
-    test('It should return no tax when taxableIncome is lte 0', async ({ page }) => {
+    for (const c of testData.salary) {
+        test(`Net salary is calculated correctly for gross = ${c.luongGross.toLocaleString('vi-VN')} (${c.desc})`, async ({ page }) => {
+            await incomeInput.fill(c.luongGross.toString());
+            await grossSangNetBtn.click();
 
-        await incomeInput.click();
-        await incomeInput.fill(testData.users.notax.luongGross.toString());
 
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('5.000.000 ₫');
-        await expect(baoHiem).toHaveText('525.000 ₫');
-        await expect(thueTNCN).toHaveText('0 ₫');
-        await expect(luongNet).toHaveText('4.475.000 ₫');
-    })
-
-    test('It applies tax rate 1 only', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill(testData.users.lv1.luongGross.toString());
-
-        await grossSangNetBtn.click();
-        await expect(luongGross).toHaveText('12.300.000 ₫');
-        await expect(baoHiem).toHaveText('1.291.500 ₫');
-        await expect(thueTNCN).toHaveText('425 ₫');
-        await expect(luongNet).toHaveText('11.008.075 ₫');
-    })
-
-    test('It applies 2 first tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('17_900_000');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('17.900.000 ₫');
-        await expect(baoHiem).toHaveText('1.879.500 ₫');
-        await expect(thueTNCN).toHaveText('252.050 ₫');
-        await expect(luongNet).toHaveText('15.768.450 ₫');
-    })
-
-    test('It applies 3 first tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('23_463_687');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('23.463.687 ₫');
-        await expect(baoHiem).toHaveText('2.463.687 ₫');
-        await expect(thueTNCN).toHaveText('750.000 ₫');
-        await expect(luongNet).toHaveText('20.250.000 ₫');
-    })
-
-    test('It applies 4 first tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('32.402.235');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('32.402.235 ₫');
-        await expect(baoHiem).toHaveText('3.402.235 ₫');
-        await expect(thueTNCN).toHaveText('1.950.000 ₫');
-        await expect(luongNet).toHaveText('27.050.000 ₫');
-    })
-    test('It applies 5 first tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('48.044.693');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('48.044.693 ₫');
-        await expect(baoHiem).toHaveText('4.926.447 ₫');
-        await expect(thueTNCN).toHaveText('4.779.562 ₫');
-        await expect(luongNet).toHaveText('38.338.685 ₫');
-    })
-
-    test('It applies 6 first tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('70.391.061');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('70.391.061 ₫');
-        await expect(baoHiem).toHaveText('5.149.911 ₫');
-        await expect(thueTNCN).toHaveText('10.422.345 ₫');
-        await expect(luongNet).toHaveText('54.818.805 ₫');
-    })
-
-    test('It applies all tax rates', async ({ page }) => {
-
-        await incomeInput.click();
-        await incomeInput.fill('101.675.978');
-
-        await grossSangNetBtn.click();
-
-        await expect(luongGross).toHaveText('101.675.978 ₫');
-        await expect(baoHiem).toHaveText('5.438.000 ₫');
-        await expect(thueTNCN).toHaveText('19.983.292 ₫');
-        await expect(luongNet).toHaveText('76.254.686 ₫');
-    })
-
+            await expect(luongGross).toHaveText(c.expected.luongGross);
+            await expect(baoHiem).toHaveText(c.expected.baoHiem);
+            await expect(thueTNCN).toHaveText(c.expected.thueTNCN);
+            await expect(luongNet).toHaveText(c.expected.luongNet);
+        });
+    }
 })
--- src/utils/testdata.js
+++ src/utils/testdata.js
@@ -1,30 +1,85 @@
 export const testData = {
-    users: {
-        notax: {
+    salary: [
+        {
+            desc: "No tax",
             luongGross: 5_000_000,
+            expected: {
+                luongGross: "5.000.000 ₫",
+                baoHiem: "525.000 ₫",
+                thueTNCN: "0 ₫",
+                luongNet: "4.475.000 ₫"
+            }
         },
-        lv1: {
+        {
+            desc: "lv1",
             luongGross: 12_300_000,
+            expected: {
+                luongGross: "12.300.000 ₫",
+                baoHiem: "1.291.500 ₫",
+                thueTNCN: "425 ₫",
+                luongNet: "11.008.075 ₫"
+            }
         },
-        lv2: {
+        {
+            desc: "lv2",
             luongGross: 17_900_000,
+            expected: {
+                luongGross: "17.900.000 ₫",
+                baoHiem: "1.879.500 ₫",
+                thueTNCN: "252.050 ₫",
+                luongNet: "15.768.450 ₫"
+            }
         },
-        lv3: {
-            luongGross: 23_463_687
+        {
+            desc: "lv3",
+            luongGross: 23_463_687,
+            expected: {
+                luongGross: "23.463.687 ₫",
+                baoHiem: "2.463.687 ₫",
+                thueTNCN: "750.000 ₫",
+                luongNet: "20.250.000 ₫"
+            }
         },
-        lv4: {
-            luongGross: 32_402_235
+        {
+            desc: "lv4",
+            luongGross: 32_402_235,
+            expected: {
+                luongGross: "32.402.235 ₫",
+                baoHiem: "3.402.235 ₫",
+                thueTNCN: "1.950.000 ₫",
+                luongNet: "27.050.000 ₫"
+            }
+        },
+        {
+            desc: "lv5",
+            luongGross: 48_044_693,
+            expected: {
+                luongGross: "48.044.693 ₫",
+                baoHiem: "4.926.447 ₫",
+                thueTNCN: "4.779.562 ₫",
+                luongNet: "38.338.685 ₫"
+            }
+        },
+        {
+            desc: "lv6",
+            luongGross: 70_391_061,
+            expected: {
+                luongGross: "70.391.061 ₫",
+                baoHiem: "5.149.911 ₫",
+                thueTNCN: "10.422.345 ₫",
+                luongNet: "54.818.805 ₫"
+            }
+        },
+        {
+            desc: "lv7",
+            luongGross: 101_675_978,
+            expected: {
+                luongGross: "101.675.978 ₫",
+                baoHiem: "5.438.000 ₫",
+                thueTNCN: "19.983.292 ₫",
+                luongNet: "76.254.686 ₫"
+            },
+        },
+    ]
 
-        },
-        lv5: {
-            luongGross: 48_044_693
-        },
-        lv6: {
-            luongGross: 70_391_061
-        },
-        lv7: {
-            luongGross: 101_675_978 
-
-}
-}
 }
